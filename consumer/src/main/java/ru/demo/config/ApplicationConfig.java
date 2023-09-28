3883546a60d71846b12fd10f955b198039b6efac
EFKO\i.sobol <i.sobol@efko.ru>
2023-09-28 16:15:31 +0300

    upd

--- consumer/src/main/java/ru/demo/config/ApplicationConfig.java
+++ consumer/src/main/java/ru/demo/config/ApplicationConfig.java
@@ -1,79 +1,58 @@
 package ru.demo.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.config.KafkaListenerContainerFactory;
 import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
-import org.springframework.kafka.support.JacksonUtils;
-import org.springframework.kafka.support.serializer.JsonDeserializer;
-import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.scheduling.concurrent.ConcurrentTaskExecutor;
-import ru.demo.model.StringValue;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 import ru.demo.service.SoapRequestSender;
-import ru.demo.service.stringValue.StringValueConsumer;
-import ru.demo.service.stringValue.StringValueConsumerLogger;
 
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
-import static org.springframework.kafka.support.serializer.JsonDeserializer.TYPE_MAPPINGS;
 
+@EnableKafka
 @Configuration
 public class ApplicationConfig {
     private static final Logger log = LoggerFactory.getLogger(ApplicationConfig.class);
     public final String topicName;
 
-    public ApplicationConfig(@Value("${application.kafka.topic}") String topicName) {
+    public ApplicationConfig(@Value("${spring.kafka.topic}") String topicName) {
         this.topicName = topicName;
     }
 
-    @Bean
-    public ObjectMapper objectMapper() {
-        return JacksonUtils.enhancedObjectMapper();
-    }
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServersConfig;
+    @Value("${spring.kafka.consumer.group-id}")
+    private String groupID;
 
     @Bean
-    public ConsumerFactory<String, StringValue> consumerFactory(
-            KafkaProperties kafkaProperties, ObjectMapper mapper) {
-        var props = kafkaProperties.buildConsumerProperties();
+    public ConsumerFactory<String, String> productConsumerFactory() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServersConfig);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, groupID);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
-        props.put(TYPE_MAPPINGS, "ru.demo.model.StringValue:ru.demo.model.StringValue");
-        props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 3);
-        props.put(ConsumerConfig.MAX_POLL_INTERVAL_MS_CONFIG, 3_000);
-
-        var kafkaConsumerFactory = new DefaultKafkaConsumerFactory<String, StringValue>(props);
-        kafkaConsumerFactory.setValueDeserializer(new JsonDeserializer<>(mapper));
-        return kafkaConsumerFactory;
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), new StringDeserializer());
     }
 
-    @Bean("listenerContainerFactory")
-    public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, StringValue>>
-    listenerContainerFactory(ConsumerFactory<String, StringValue> consumerFactory) {
-        var factory = new ConcurrentKafkaListenerContainerFactory<String, StringValue>();
-        factory.setConsumerFactory(consumerFactory);
-        factory.setBatchListener(true);
-        factory.setConcurrency(1);
-        factory.getContainerProperties().setIdleBetweenPolls(1_000);
-        factory.getContainerProperties().setPollTimeout(1_000);
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
 
-        var executor = new SimpleAsyncTaskExecutor("k-consumer-");
-        executor.setConcurrencyLimit(10);
-        var listenerTaskExecutor = new ConcurrentTaskExecutor(executor);
-        factory.getContainerProperties().setListenerTaskExecutor(listenerTaskExecutor);
+        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(productConsumerFactory());
+
         return factory;
     }
 
@@ -82,36 +61,28 @@ public class ApplicationConfig {
         return TopicBuilder.name(topicName).partitions(1).replicas(1).build();
     }
 
-    @Bean
-    public StringValueConsumer stringValueConsumerLogger() {
-        return new StringValueConsumerLogger();
-    }
 
     @Bean
-    public KafkaClient stringValueConsumer(StringValueConsumer stringValueConsumer, SoapRequestSender soapRequest) {
-        return new KafkaClient(stringValueConsumer, soapRequest);
+    public KafkaClient stringValueConsumer(SoapRequestSender soapRequest) {
+        return new KafkaClient(soapRequest);
     }
+
 
     public static class KafkaClient {
-        private final StringValueConsumer stringValueConsumer;
         private final SoapRequestSender soapRequestSender;
 
 
-
-        public KafkaClient(StringValueConsumer stringValueConsumer, SoapRequestSender soapRequestSender) {
-            this.stringValueConsumer = stringValueConsumer;
+        public KafkaClient(SoapRequestSender soapRequestSender) {
             this.soapRequestSender = soapRequestSender;
         }
 
-        @KafkaListener(
-                topics = "${application.kafka.topic}",
-                containerFactory = "listenerContainerFactory")
-        public void listen(@Payload List<StringValue> values) throws Exception {
-            log.info("values, values.size:{}", values.size());
-            stringValueConsumer.accept(values);
-            for(StringValue value: values){
-                soapRequestSender.choiceOfMethodAction(value);
-            }
+        @KafkaListener(topics = "${spring.kafka.topic}", groupId = "${spring.kafka.consumer.group-id}")
+        public void consume(ConsumerRecord<String, String> record) throws Exception {
+            String key = record.key();
+            String value = record.value();
+            log.info(String.format("key -> " + key + ", value -> " + value));
+            soapRequestSender.choiceOfMethodAction(key, value);
         }
+
     }
 }
